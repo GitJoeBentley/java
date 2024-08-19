d4efdfd3c951c30f46da4789dd938a7229aad821
GitJoeBentley <88294868+GitJoeBentley@users.noreply.github.com>
2024-08-19 15:56:24 -0700

    Add files via upload
    Allow only words with alphabetic characters

--- mapwordcounter/MapWordCounter.java
+++ mapwordcounter/MapWordCounter.java
@@ -2,6 +2,10 @@ package joe.mapwordcounter;
 
 public class MapWordCounter {
 
+    public static boolean isStringOnlyAlphabet(String str) {
+        return ((str != null) && (!str.equals(""))
+            && (str.matches("^[a-zA-Z]*$")));
+    }
     public static void main(String[] args) {
         Words words = new Words("c:/temp/king-james.txt");
         words.print();      
